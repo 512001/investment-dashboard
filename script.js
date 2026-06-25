31a013c87444a58172f072a3b5f82686ecfc1b8f
Ajith Kumar M <69567505+512001@users.noreply.github.com>
2026-06-25 17:02:45 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-function filterTable(){
-let input=document.getElementById('search').value.toLowerCase();
-let rows=document.querySelectorAll('table tbody tr');
-rows.forEach(r=>{
-r.style.display=r.innerText.toLowerCase().includes(input)?'':'none';
-});
-}
