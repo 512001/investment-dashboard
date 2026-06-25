5ee277e8a121879ff79194888eb56175aa820ef3
Ajith Kumar M <69567505+512001@users.noreply.github.com>
2026-06-25 16:42:45 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-function calcSIP(){
-let p=parseFloat(document.getElementById('sip').value)||0;
-let r=(parseFloat(document.getElementById('rate').value)||0)/1200;
-let n=(parseFloat(document.getElementById('years').value)||0)*12;
-let fv=p*((Math.pow(1+r,n)-1)/r)*(1+r);
-document.getElementById('result').innerText='Estimated Corpus: ₹'+fv.toFixed(0);
-}
